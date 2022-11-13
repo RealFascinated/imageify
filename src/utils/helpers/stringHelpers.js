2b9dbd0e8359512cbf47e0e9183e1cc94586b9bc
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-11-13 20:19:47 +0000

    add string helpers

---
+++ src/utils/helpers/stringHelpers.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a randomly generated string with the specified length
+ *
+ * @param {Number} length
+ * @returns The random string
+ */
+function randomString(length) {
+	var result = "";
+	var characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
