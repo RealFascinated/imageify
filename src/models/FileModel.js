511e4875161abe8e3b90a00077c8408830cd23bb
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-11-14 00:08:54 +0000

    Update file model

--- src/models/FileModel.js
+++ src/models/FileModel.js
@@ -5,6 +5,11 @@ const schema = new Schema({
 	fileId: String,
 	uploadDate: Date,
 	contentType: String,
+	ext: String,
+
+	// Image/Video specific properties
+	width: Number, // The width of the image/video in pixels
+	height: Number, // The height of the image/video in pixels
 });
 
 export default mongoose.models.File || mongoose.model("File", schema);
