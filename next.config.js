f5c22f5260389eb341188dd63f5091457e89972c
Liam <67254223+RealFascinated@users.noreply.github.com>
2022-11-14 02:32:02 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	//output: "standalone",
+	swcMinify: true,
+	optimizeFonts: true,
 
 	images: {
 		remotePatterns: [
