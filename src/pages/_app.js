b17848fdfb45d96737360adf72fa5f3d43d8d31f
Liam <admin@fascinated.cc>
2022-11-17 05:58:59 +0000

    Add next-seo template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,22 @@ import "../styles/globals.css";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
+import { NextSeo } from "next-seo";
 import Layout from "src/components/Layout";
+import { darkTheme } from "src/consts/themes";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
-		<Layout>
-			<SessionProvider session={session}>
-				<NextUIProvider>
-					<Component {...pageProps} />
-				</NextUIProvider>
-			</SessionProvider>
-		</Layout>
+		<NextUIProvider theme={darkTheme}>
+			<NextSeo titleTemplate="%s | Imageify" />
+			<Layout>
+				<SessionProvider session={session}>
+					<NextUIProvider>
+						<Component {...pageProps} />
+					</NextUIProvider>
+				</SessionProvider>
+			</Layout>
+		</NextUIProvider>
 	);
 }
 
