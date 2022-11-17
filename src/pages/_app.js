4ccba7323d47bdb0154e7060f560fae1d980b055
Liam <admin@fascinated.cc>
2022-11-17 07:16:29 +0000

    Add base opengraph layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,12 +4,17 @@ import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { NextSeo } from "next-seo";
 import Layout from "src/components/Layout";
+import { baseOpenGraph } from "src/consts/openGraphBase";
 import { darkTheme } from "src/consts/themes";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<NextUIProvider theme={darkTheme}>
-			<NextSeo titleTemplate="%s | Imageify" />
+			<NextSeo
+				titleTemplate="%s | Imageify"
+				themeColor="#0063EB"
+				openGraph={baseOpenGraph}
+			/>
 			<Layout>
 				<SessionProvider session={session}>
 					<NextUIProvider>
