0cd7e1e3dfc33d062d4f725eb9a2c3f419eb49e6
Liam <admin@fascinated.cc>
2022-11-19 10:45:46 +0000

    Add check for if we should show default metadata

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,21 @@ import "../styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
 import Layout from "src/components/Layout";
+import { baseOpenGraph } from "src/consts/openGraphBase";
 import { darkTheme } from "src/consts/themes";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	const router = useRouter();
+	const shouldReturnMetadata = router.pathname !== "/files/[fileId]";
+
 	return (
 		<NextUIProvider theme={darkTheme}>
 			<NextSeo
 				titleTemplate="%s | Imageify"
 				themeColor="#0063EB"
-				//openGraph={baseOpenGraph}
+				openGraph={shouldReturnMetadata ? baseOpenGraph : undefined}
 			/>
 			<Layout>
 				<SessionProvider session={session}>
