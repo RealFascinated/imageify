0047f31cd508fa56531965be5ba2f2b28dabbc62
Liam <admin@fascinated.cc>
2022-11-19 11:06:51 +0000

    Add basic front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
+import { Button, Text } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Home() {
-	return <></>;
+	return (
+		<div className="h-screen flex flex-col items-center justify-center">
+			<Text h1 className="font-bold text-4xl">
+				Imageify
+			</Text>
+			<Text className="mt-2 text-xl">
+				Free and open-source ShareX image host that you can self-host!
+			</Text>
+
+			<div className="mt-5">
+				<Button auto className="bg-blue-600">
+					<Link href="https://github.com/RealFascinated/imageify">Github</Link>
+				</Button>
+			</div>
+		</div>
+	);
 }
